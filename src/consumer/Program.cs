c26f8a8940026eea96528a7cdcfc1a6beedf4472
Ivan Lutsenko <evanlt187@gmail.com>
2024-03-04 15:59:53 +0200

    Added basic poc

---
+++ src/consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "test",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (ch, ea) =>
+{
+    var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+			
+    Console.WriteLine($"Consooomed the {content}");
+
+    channel.BasicAck(ea.DeliveryTag, false);
+};
+
+channel.BasicConsume("test", false, consumer);
+
+Console.ReadLine();
---
+++ src/publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var message = "Hello from publisher";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "test",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "",
+    routingKey: "test",
+    basicProperties: null,
+    body: body);
